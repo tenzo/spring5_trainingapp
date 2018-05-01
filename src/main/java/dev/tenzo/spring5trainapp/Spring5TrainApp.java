6dad287e67df3dfb6fba258cd006fb01df77fbf3
tenzodev <tenzo.dev@gmail.com>
2018-05-01 12:05:09 +0200

    Init commit, gradle + basic setup

---
+++ src/main/java/dev/tenzo/spring5trainapp/Spring5TrainApp.java
@@ -0,0 +1,12 @@
+package dev.tenzo.spring5trainapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5TrainApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5TrainApp.class, args);
+	}
+}
---
+++ src/test/java/dev/tenzo/spring5trainapp/Spring5TrainAppTests.java
@@ -0,0 +1,16 @@
+package dev.tenzo.spring5trainapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5TrainAppTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
