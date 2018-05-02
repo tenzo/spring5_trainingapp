d288baf7d0da81c634c0d423e237151e49dc0c60
tenzodev <tenzo.dev@gmail.com>
2018-05-02 20:14:30 +0200

    added thymeleaf templates

---
+++ src/main/java/dev/tenzo/spring5trainapp/controller/AuthorController.java
@@ -0,0 +1,25 @@
+package dev.tenzo.spring5trainapp.controller;
+
+import dev.tenzo.spring5trainapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors";
+
+    }
+}
