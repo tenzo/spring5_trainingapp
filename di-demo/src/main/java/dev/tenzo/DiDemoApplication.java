ac2cb1a370677530d631d598985c4d787069d4e5
tenzodev <tenzo.dev@gmail.com>
2018-05-02 21:09:05 +0200

    Demo of spring context bean retrieve.

---
+++ di-demo/src/main/java/dev/tenzo/DiDemoApplication.java
@@ -0,0 +1,19 @@
+package dev.tenzo;
+
+import dev.tenzo.controller.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+
+        MyController myController = (MyController) ctx.getBean("myController");
+
+        myController.sayHello();
+    }
+}
---
+++ di-demo/src/main/java/dev/tenzo/controller/MyController.java
@@ -0,0 +1,12 @@
+package dev.tenzo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public void sayHello() {
+        System.out.println("Hello!!!");
+    }
+
+}
